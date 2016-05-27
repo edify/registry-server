79065f57f8c52710ca5d1b9517088684d3483e80
Jasson Cascante <jasson.cascante@edify.cr>
2016-05-26 21:01:36 -0600

    replaced theme manager url

--- controller/upsertResume.js
+++ controller/upsertResume.js
@@ -1,83 +1,82 @@
-var bcrypt = require('bcrypt-nodejs');
-var HttpStatus = require('http-status-codes');
-var User = require('../models/user');
-var Resume = require('../models/resume');
+var bcrypt = require('bcrypt-nodejs')
+var HttpStatus = require('http-status-codes')
+var User = require('../models/user')
+var Resume = require('../models/resume')
 
-function S4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-};
+function S4 () {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1)
+}
 
-module.exports = function upsertResume(req, res, next) {
+module.exports = function upsertResume (req, res, next) {
+  var redis = req.redis
 
-    var redis = req.redis;
+  var password = req.body.password
+  var email = req.body.email || req.session.email
 
-    var password = req.body.password;
-    var email = req.body.email || req.session.email;
+  if (!req.body.guest) {
+    User.findOne({
+      'email': email
+    }, function (err, user) {
+      if (err) {
+        return next(err)
+      }
 
-    if (!req.body.guest) {
-        User.findOne({
-            'email': email
-        }, function(err, user) {
-          if (err) {
-            return next(err);
-          }
+      redis.get(req.body.session, function (err, valueExists) {
+        var respondWithResume = function () {}
 
-            redis.get(req.body.session, function(err, valueExists) {
-                var respondWithResume = function() {
-
-                };
-
-                if ((user && password && bcrypt.compareSync(password, user.hash)) || (typeof req.session.username !== 'undefined') || valueExists) {
-                    var resume = req.body && req.body.resume || {};
-                    resume.jsonresume = {
-                        username: user.username,
-                        passphrase: req.body.passphrase || null,
-                        theme: req.body.theme || null
-                    };
-
-                    var conditions = {
-                        'jsonresume.username': user.username
-                    };
-
-                    Resume.update(conditions, resume, { upsert: true }, function(err, resume) {
-                        if (err) {
-                            return next(err);
-                        }
-
-                        res.send({
-                            url: 'http://registry.jsonresume.org/' + user.username
-                        });
-                    });
-                } else if (valueExists === null) {
-                    res.status(HttpStatus.UNAUTHORIZED).send({
-                        sessionError: 'invalid session'
-                    });
-                } else {
-                    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
-                        message: 'ERRORRRSSSS'
-                    });
-                }
-            });
-        });
-    } else {
-        var guestUsername = S4() + S4();
-        var resume = req.body && req.body.resume || {};
-        resume.jsonresume = {
-            username: guestUsername,
+        if ((user && password && bcrypt.compareSync(password, user.hash)) || (typeof req.session.username !== 'undefined') || valueExists) {
+          var resume = req.body && req.body.resume || {}
+          resume.jsonresume = {
+            username: user.username,
             passphrase: req.body.passphrase || null,
             theme: req.body.theme || null
-        };
+          }
 
-        Resume.create(resume, function(err, resume) {
+          var conditions = {
+            'jsonresume.username': user.username
+          }
+
+          Resume.update(conditions, resume, {
+            upsert: true
+          }, function (err, resume) {
             if (err) {
-                return next(err);
+              return next(err)
             }
 
             res.send({
-                url: 'http://registry.jsonresume.org/' + guestUsername
-            });
-        });
+              url: 'http://resumes.edify.cr/' + user.username
+            })
+          })
+        } else if (valueExists === null) {
+          res.status(HttpStatus.UNAUTHORIZED).send({
+            sessionError: 'invalid session'
+          })
+        } else {
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+            message: 'ERRORRRSSSS'
+          })
+        }
+      })
+    })
+  } else {
+    var guestUsername = S4() + S4()
+    var resume = req.body && req.body.resume || {}
+    resume.jsonresume = {
+      username: guestUsername,
+      passphrase: req.body.passphrase || null,
+      theme: req.body.theme || null
     }
-};
+
+    Resume.create(resume, function (err, resume) {
+      if (err) {
+        return next(err)
+      }
+
+      res.send({
+        url: 'http://resumes.edify.cr/' + guestUsername
+      })
+    })
+  }
+}
